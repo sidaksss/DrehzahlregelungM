bd6e80c07cdd568a8baf135d9298a22abfe27890
sidaksss <147280011+sidaksss@users.noreply.github.com>
2023-10-09 19:23:27 +0200

    new commit

--- Erweitertes_System.m
+++ Erweitertes_System.m
@@ -5,7 +5,7 @@ function [dydt] = Erweitertes_System(t, y, params, Isoll, Isolldot)
     K0 = params.K0;
     K1 = params.K1;
     Kp = params.Kp;
-    Kv = params.Kv;
+    Kv = params.Kv; %Parameter attain Kv
     
     % Anfangsdifferentialgleichung (1) mit ersetztem Strom durch Drehzahlen
     dydt = [y(2); Kv*K1*Isolldot(t) + Kv*K0*Isoll(t)-a1*y(2)-a0*y(1)];
